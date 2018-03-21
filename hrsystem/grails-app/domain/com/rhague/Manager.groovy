1142153a9b5fae416a4fb97fc460cb6ad00d8711
b6016323 <b6016323@my.shu.ac.uk>
2018-03-21 16:23:04 +0000

    flase to false again

--- hrsystem/grails-app/domain/com/rhague/Manager.groovy
+++ hrsystem/grails-app/domain/com/rhague/Manager.groovy
@@ -8,11 +8,11 @@ class Manager {
     string office
     String department
     static constraints = {
-        fullName nullable:flase, blank:false
-        userName nullable:flase, blank:false, unique:true
-        password nullable:flase, blank:false
-        managerEmail nullable:flase, blank:false, email:true
-        office nullable:flase, blank:false
-        department nullable:flase, blank:false
+        fullName nullable:false, blank:false
+        userName nullable:false, blank:false, unique:true
+        password nullable:false, blank:false
+        managerEmail nullable:false, blank:false, email:true
+        office nullable:false, blank:false
+        department nullable:false, blank:false
     }
 }
