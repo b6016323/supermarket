c4b2588e0d084724552df19372ee7ebf80374323
b6016323 <b6016323@my.shu.ac.uk>
2018-03-21 16:14:31 +0000

    gedit thing

--- hrsystem/grails-app/domain/com/rhague/Shift.groovy
+++ hrsystem/grails-app/domain/com/rhague/Shift.groovy
@@ -1,7 +1,14 @@
 package com.rhague
 
 class Shift {
-
+    String timeOfDay
+    String dayOfWeek
+    int numberOfHours
+    double startingTime
     static constraints = {
+        timeOfDay nullable:flase, blank:false
+        dayOfWeek nullable:flase, blank:false, inList:["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+        numberOfHours nullable:flase, blank:false, max:24
+        startingTime nullable:flase, blank:false, range:0..24
     }
 }
