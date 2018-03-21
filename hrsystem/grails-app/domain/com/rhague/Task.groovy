b16c5e883b2500594e77f41670d6115fdefb83c0
b6016323 <b6016323@my.shu.ac.uk>
2018-03-21 16:18:08 +0000

    set task settings

--- hrsystem/grails-app/domain/com/rhague/Task.groovy
+++ hrsystem/grails-app/domain/com/rhague/Task.groovy
@@ -1,7 +1,22 @@
 package com.rhague
 
 class Task {
-
+    int taskID
+    String taskName
+    int numberOfPeople
+    String sectionName
+    String department
+    String timeRequired
+    String description
+    boolean taskCompleted
     static constraints = {
+        taskID nullable:false, blank:false, unique:true
+        taskName nullable:false, blank:false
+        numberOfPeople nullable:false, blank:false, min:0
+        sectionName nullable:false, blank:false
+        department nullable:false, blank:false
+        timeRequired nullable:false, blank:false
+        description nullable:false, blank:false
+        taskCompleted nullable:false, blank:false
     }
 }
