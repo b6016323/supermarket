79e5f2e91b38423cda644f135422c2318838333e
b6016323 <b6016323@my.shu.ac.uk>
2018-03-21 16:19:50 +0000

    setting team

--- hrsystem/grails-app/domain/com/rhague/Team.groovy
+++ hrsystem/grails-app/domain/com/rhague/Team.groovy
@@ -1,7 +1,16 @@
 package com.rhague
 
 class Team {
-
+    ID teamID
+    String teamName
+    int numberOfEmployees
+    Section sectionName
+    Section description
     static constraints = {
+        teamID nullable:false, blank:false, unique:true
+        teamName nullable:false, blank:false
+        numberOfEmployees nullable:false, blank:false
+        sectionName nullable:false, blank:false
+        description nullable:false, blank:false, widget:'textarea'
     }
 }
