52225e7bbc51a109d5a7792801829f14579c9266
b6016323 <b6016323@my.shu.ac.uk>
2018-03-21 16:09:49 +0000

    Gedit is a pain to search and type

--- hrsystem/grails-app/domain/com/rhague/Manager.groovy
+++ hrsystem/grails-app/domain/com/rhague/Manager.groovy
@@ -1,7 +1,18 @@
 package com.rhague
 
 class Manager {
-
+    String fullName
+    String userName
+    String password
+    String managerEmail
+    string office
+    String department
     static constraints = {
+        fullName nullable:flase, blank:false
+        userName nullable:flase, blank:false, unique:true
+        password nullable:flase, blank:false
+        managerEmail nullable:flase, blank:false, email:true
+        office nullable:flase, blank:false
+        department nullable:flase, blank:false
     }
 }
