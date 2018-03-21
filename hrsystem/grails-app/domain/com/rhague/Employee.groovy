b2df0f474939b89ea917e4fcfbae05469ce22448
b6016323 <b6016323@my.shu.ac.uk>
2018-03-21 16:04:59 +0000

    easier to type than in gedit

--- hrsystem/grails-app/domain/com/rhague/Employee.groovy
+++ hrsystem/grails-app/domain/com/rhague/Employee.groovy
@@ -1,7 +1,22 @@
 package com.rhague
 
 class Employee {
-
+    String fullName
+    Date dateOfBirth
+    String residence
+    double hourlyRate
+    String employeeID
+    Date dateEmployed
+    String taxCode
+    String contract
     static constraints = {
+        fullName nullable:flase, blank:false
+        dateOfBirth nullable:flase, blank:false
+        residence nullable:flase, blank:false
+        hourlyRate nullable:flase, blank:false, min:0.00
+        employeeID nullable:flase, blank:false, unique:true
+        dateEmployed nullable:flase, blank:false
+        taxCode fullName nullable:flase, blank:false
+        contract fullName nullable:flase, blank:false
     }
 }
