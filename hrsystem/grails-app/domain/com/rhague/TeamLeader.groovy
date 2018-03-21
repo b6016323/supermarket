0460f8cc59418dc83bdfb179b7249659814ce7d7
b6016323 <b6016323@my.shu.ac.uk>
2018-03-21 16:21:52 +0000

    setting teamleader

--- hrsystem/grails-app/domain/com/rhague/TeamLeader.groovy
+++ hrsystem/grails-app/domain/com/rhague/TeamLeader.groovy
@@ -1,7 +1,20 @@
 package com.rhague
 
 class TeamLeader {
-
+    String fullName
+    String department
+    String employeeID
+    String sectionName
+    long officePhone
+    String leaderEmail
+    String password
     static constraints = {
+        fullName nullable:false, blank:false
+        department nullable:false, blank:false
+        employeedID nullable:false, blank:false,unique:true
+        sectionName nullable:false, blank:false
+        officePhone nullable:false, blank:false
+        leaderEmail nullable:false, blank:false,email:true
+        password nullable:false, blank:false
     }
 }
