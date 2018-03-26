c3e4a0c88ccc3d16020d49e8c966df3200dd75b0
b6016323 <b6016323@my.shu.ac.uk>
2018-03-26 12:39:47 +0100

    commit before break

--- hrsystem/grails-app/domain/com/rhague/Shift.groovy
+++ hrsystem/grails-app/domain/com/rhague/Shift.groovy
@@ -15,6 +15,6 @@ class Shift {
         timeOfDay nullable:false, blank:false
         dayOfWeek nullable:false, blank:false, inList:["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
         numberOfHours nullable:false, blank:false, max:24
-        startingTime nullable:false, blank:false, range:0..24
+        startingTime nullable:false, blank:false, min:0d, max:24d
     }
 }
